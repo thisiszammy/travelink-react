bc49ba0eb8c53eb4931969eec34eb49322e42414
Jay <thisiszammy@gmail.com>
2024-07-05 16:04:39 +0800

    [zammy][branch-merge] merged with latest auth changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import '@mantine/core/styles.css';
 import Login from './pages/loginPage/Login';
 import Register from './pages/registerPage/Register';
+import CatalogPage from './pages/catalogPage/CatalogPage';
+import TravelinkAppShell from './components/TravelinkAppShell';
+import { MantineProvider } from '@mantine/core';
 
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/catalog' />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
+    <MantineProvider>
+      <TravelinkAppShell/>
+    </MantineProvider>
   );
 };
 
