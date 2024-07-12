4b87a10861ee6ac34036eae78200926ef50dc89a
farley16 <farleyesmero0@gmail.com>
2024-07-12 16:08:10 +0800

    Added AccountSamplePage, BookingSamplePage, NavigationBar, TopBar, LeftDrawerBar components

---
+++ src/pages/profilePage/Booking.tsx
@@ -0,0 +1,18 @@
+// src/pages/profilePage/Booking.tsx
+import React from 'react';
+import Drawer from '../../components/Drawer';
+import './Booking.css';
+
+const Booking: React.FC = () => {
+  return (
+    <div>
+      <div className="booking-content">
+       <Drawer />
+      <h1>Booking Page</h1>
+      <p>This is the Booking page content.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Booking;
