34b08e7db7cd77b085675123996b9d6a2839efbb
Jay <thisiszammy@gmail.com>
2024-07-12 17:57:51 +0800

    [zammy][Components] Landing Page banner & header starting build

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { MantineProvider } from '@mantine/core';
+import './res/styles/globals.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
